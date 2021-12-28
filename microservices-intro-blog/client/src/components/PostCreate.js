50bdbda1b6c2e7d2e2b22600e6e3687de7c5935d
Rob de Boer <roblkdeboer@gmail.com>
2021-12-28 16:48:17 +0800

    Set up App and create a post component

---
+++ microservices-intro-blog/client/src/components/PostCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PostCreate = () => {
+  const [title, setTitle] = useState('');
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title,
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control my-3"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
