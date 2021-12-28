ff6e2d0cac1b6ef5478d6ee100349f25bb56ae51
Rob de Boer <roblkdeboer@gmail.com>
2021-12-28 17:17:54 +0800

    Create post list component

--- microservices-intro-blog/client/src/App.js
+++ microservices-intro-blog/client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PostCreate from './components/PostCreate';
+import PostList from './components/PostList';
 
 const App = () => {
   return (
     <div className="container">
-      <h1>Create Post</h1>
+      <h1 className="mt-3">Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
