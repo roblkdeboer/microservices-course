93af1d102d6ac5b4fec000b933cfd7b4959c5a43
Rob de Boer <roblkdeboer@gmail.com>
2021-12-29 13:57:42 +0800

    Create comment list component

--- microservices-intro-blog/client/src/components/PostList.js
+++ microservices-intro-blog/client/src/components/PostList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -7,6 +8,7 @@ const PostList = () => {
   const fetchPosts = async () => {
     const res = await axios.get('http://localhost:4000/posts');
 
+    // Posts are returned from the endpoint and assigned to posts variable
     setPosts(res.data);
   };
 
@@ -15,15 +17,13 @@ const PostList = () => {
   }, []);
 
   //   An  array of post objects
+  //   Each post has an id that is returned from the API
   const renderedPosts = Object.values(posts).map((post) => {
     return (
-      <div
-        className="card"
-        style={{ width: '30%', marginBottom: '20px' }}
-        key={post.id}
-      >
+      <div className="card my-3" style={{ width: '30%' }} key={post.id}>
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
