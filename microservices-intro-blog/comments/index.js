77066a18cddb402bbe6309c01e1314fb249dfefa
Rob de Boer <roblkdeboer@gmail.com>
2021-12-29 18:27:22 +0800

    Receive events from post and comment services

--- microservices-intro-blog/comments/index.js
+++ microservices-intro-blog/comments/index.js
@@ -37,6 +37,13 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
+// Receive events
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+});
+
 app.listen(4001, () => {
   console.log('Listening on 4001');
 });
--- microservices-intro-blog/posts/index.js
+++ microservices-intro-blog/posts/index.js
@@ -34,6 +34,13 @@ app.post('/posts', async (req, res) => {
   res.status(201).send(posts[id]);
 });
 
+// Receive events
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+});
+
 app.listen(4000, () => {
   console.log('Listening on 4000');
 });
