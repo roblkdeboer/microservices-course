f35f77c531d7d9478aa727188fc101658b7aca33
Rob de Boer <roblkdeboer@gmail.com>
2022-01-05 03:01:42 +0800

    Add skaffold for kubernetes management

--- microservices-intro-blog/posts/index.js
+++ microservices-intro-blog/posts/index.js
@@ -42,6 +42,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('V55 - Test changes');
+  console.log('V1000 - Test changes');
   console.log('Listening on 4000');
 });
