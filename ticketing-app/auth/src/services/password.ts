c730b9deed110ea0fc0eb1b9634a31313a63bbf0
Rob de Boer <roblkdeboer@gmail.com>
2022-01-16 00:48:49 +0800

    Create password comparing service

--- ticketing-app/auth/src/services/password.ts
+++ ticketing-app/auth/src/services/password.ts
@@ -12,5 +12,11 @@ export class Password {
     return `${buf.toString('hex')}.${salt}`;
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    // Buffer is an array with raw data inside of it
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString('hex') === hashedPassword;
+  }
 }
