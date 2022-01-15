1c5d2639fdd1627499c5bc54fe33e9d8158ad609
Rob de Boer <roblkdeboer@gmail.com>
2022-01-16 00:02:55 +0800

    Add type checking on user document created and returned by mongodb

--- ticketing-app/auth/src/models/user.ts
+++ ticketing-app/auth/src/models/user.ts
@@ -9,8 +9,16 @@ interface UserAttrs {
 
 // An interface that describes the properties
 // that a user model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+// Returns an instance with userdoc instance
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a user document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -30,6 +38,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
 };
 
 // Use the interface above to add type definitions to the build property
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
