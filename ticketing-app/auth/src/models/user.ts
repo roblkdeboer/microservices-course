33557e91d99a3af6622edadf61b51d600900440c
Rob de Boer <roblkdeboer@gmail.com>
2022-01-15 23:56:30 +0800

    Create build method in user model to integrate type checking to user model

--- ticketing-app/auth/src/models/user.ts
+++ ticketing-app/auth/src/models/user.ts
@@ -7,6 +7,12 @@ interface UserAttrs {
   password: string;
 }
 
+// An interface that describes the properties
+// that a user model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -17,12 +23,13 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-
-const User = mongoose.model('User', userSchema);
-
 // Use the interface defined above.  Allows for type checking
-const buildUser = (attrs: UserAttrs) => {
+// How to add a function to a model in mongoose
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export { User, buildUser };
+// Use the interface above to add type definitions to the build property
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+export { User };
