c862a01523bcbf1fea4cc237f4e4665212816f18
Rob de Boer <roblkdeboer@gmail.com>
2022-01-16 22:29:16 +0800

    Add JWT creation and saving to cookie session object

--- ticketing-app/auth/src/routes/signup.ts
+++ ticketing-app/auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -37,6 +39,20 @@ router.post(
     // Save user to the DB
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'asdf'
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
