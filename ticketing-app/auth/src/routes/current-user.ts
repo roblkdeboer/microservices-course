a18f086ce5124f4a90581d30381913cc29532e6e
Rob de Boer <roblkdeboer@gmail.com>
2022-01-17 13:37:56 +0800

    Implement check if user is signed in and signout logic

--- ticketing-app/auth/src/routes/current-user.ts
+++ ticketing-app/auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+// Check whether the user has a current logged in session with a JWT
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+  if (!req.session || !req.session.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
