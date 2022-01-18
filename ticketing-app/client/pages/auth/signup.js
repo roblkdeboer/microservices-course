3ac679028144257352867ba5abe148541b548a02
Rob de Boer <roblkdeboer@gmail.com>
2022-01-18 20:19:03 +0800

    Create sign up form

---
+++ ticketing-app/client/pages/auth/signup.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={submitHandler}>
+        <h1>Sign Up</h1>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary my-3">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default signup;
