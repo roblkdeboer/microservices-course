907641b4635433bc383a8b0143f397bd1e2a07e3
Rob de Boer <roblkdeboer@gmail.com>
2022-01-19 00:08:03 +0800

    Allow for api call to be made between services via nginx

--- ticketing-app/client/pages/index.js
+++ ticketing-app/client/pages/index.js
@@ -2,19 +2,36 @@ import React from 'react';
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
-
+  //   console.log(currentUser);
+  //   axios.get('/api/users/currentuser').catch((err) => {
+  //     console.log(err.message);
+  //   });
   return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   // Check if the user is signed in/authenticated
-//   const response = await axios.get('/api/users/currentuser');
+// Executed when the page first loads
+// Executed on the server when page is refreshed, accessed via a link from a different domain or typed into the search bar
+// Executed on the client when navigating between pages within the app
 
-//   return response.data;
-// };
+LandingPage.getInitialProps = async ({ req }) => {
+  // Check if the user is signed in/authenticated
+  if (typeof window === 'undefined') {
+    // If undefined, means accessing via the server
+    // Requests are made to ingress
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        // Request header contains the cookie
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    //Accessing via the browser
+    // Requests are made directly to the service
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+};
 
 export default LandingPage;
