e2e6326b2a75aae2a7c53a1d8b030fab10f27aac
Rob de Boer <roblkdeboer@gmail.com>
2022-01-18 23:07:00 +0800

    Implement check user is logged in on landing page

--- ticketing-app/client/pages/index.js
+++ ticketing-app/client/pages/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
-const index = () => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>;
 };
 
-export default index;
+// LandingPage.getInitialProps = async () => {
+//   // Check if the user is signed in/authenticated
+//   const response = await axios.get('/api/users/currentuser');
+
+//   return response.data;
+// };
+
+export default LandingPage;
