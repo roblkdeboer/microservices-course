f411222d7e87585981f1428349c1737244d101dd
Rob de Boer <roblkdeboer@gmail.com>
2022-01-23 16:33:33 +0800

    Make MONGO_URI a env variable in k8s config files

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -8,9 +8,13 @@ const start = async () => {
     throw new Error('JWT_KEY must be defined');
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to MongoDB');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to Auth MongoDB');
   } catch (err) {
     console.log(err);
   }
