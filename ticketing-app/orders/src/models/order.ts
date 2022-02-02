206bb7e0528473079b30e3516944c295b5568605
Rob de Boer <roblkdeboer@gmail.com>
2022-02-03 00:40:19 +0800

    Set up new order creation model and validation

---
+++ ticketing-app/orders/src/models/order.ts
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@robtickets/common';
+
+// An interface that describes the properties
+// that are required to create a new order
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties
+// that a order document has
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties
+// that a order model has
+// Returns an instance with orderdoc instance
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+      required: false,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    //   Transform the response to standardise across languages
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+// Use the interface defined above.  Allows for type checking
+// How to add a function to a model in mongoose
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+// Use the interface above to add type definitions to the build property
+// Pass in the OrderDoc interface and returns a OrderModel interface
+const Order = mongoose.model<OrderDoc, OrderModel>('Order, orderSchema');
+
+export { Order };
