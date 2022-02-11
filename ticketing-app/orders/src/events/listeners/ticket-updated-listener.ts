b82d4e900cf8799c650aa2f2fa4100f348c555c4
Rob de Boer <roblkdeboer@gmail.com>
2022-02-11 19:02:33 +0800

    Implement ticket updated listener

---
+++ ticketing-app/orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketUpdatedEvent } from '@robtickets/common';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+// Generic class to listen for ticket updated event
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+
+    await ticket.save();
+
+    msg.ack();
+  }
+}
