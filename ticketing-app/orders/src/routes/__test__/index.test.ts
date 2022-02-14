774e946f2588ae4f893444192f8a8b426cb7aa25
Rob de Boer <roblkdeboer@gmail.com>
2022-02-15 00:06:25 +0800

    Fixing failing tests by passing required ID

--- ticketing-app/orders/src/routes/__test__/index.test.ts
+++ ticketing-app/orders/src/routes/__test__/index.test.ts
@@ -2,10 +2,12 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import mongoose from 'mongoose';
 
 // Helper function to create tickets
 const buildTicket = async () => {
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'Concert',
     price: 20,
   });
