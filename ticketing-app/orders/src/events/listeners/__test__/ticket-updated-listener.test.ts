f107f7cf2d393d19ca517ab46af89f88418b1883
Rob de Boer <roblkdeboer@gmail.com>
2022-02-14 23:49:03 +0800

    Add tests for ticket created updated event

---
+++ ticketing-app/orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import { Ticket } from '../../../models/ticket';
+
+import { TicketUpdatedEvent } from '@robtickets/common';
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Message } from 'node-nats-streaming';
+
+const setup = async () => {
+  // Create an instance of the listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 10,
+  });
+
+  await ticket.save();
+
+  // Create a fake update data event
+  const data: TicketUpdatedEvent['data'] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: 'new concert',
+    price: 20,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // Create a fake message object
+  //   @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, ticket, msg };
+};
+
+it('finds, updates and saves a ticket', async () => {
+  const { listener, data, ticket, msg } = await setup();
+
+  // Call the onMessage function with the data object +  message object
+  await listener.onMessage(data, msg);
+
+  // Write assertions to make sure a ticket was created
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acknowledges the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  // Call the onMessage function with the data object +  message object
+  await listener.onMessage(data, msg);
+
+  //   Write assertions to make sure ack fucntion is called
+  expect(msg.ack).toHaveBeenCalled();
+});
