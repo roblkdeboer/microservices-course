b52cc72737369e1ed8b6b9e2221b9ec2629cb127
Rob de Boer <roblkdeboer@gmail.com>
2022-02-17 22:51:07 +0800

    Implement order expiration complete listener

--- ticketing-app/orders/src/routes/new.ts
+++ ticketing-app/orders/src/routes/new.ts
@@ -16,7 +16,7 @@ import { natsWrapper } from '../nats-wrapper';
 const router = express.Router();
 
 // Expiration time for tickets
-const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+const EXPIRATION_WINDOW_SECONDS = 1 * 60;
 
 router.post(
   '/api/orders',
