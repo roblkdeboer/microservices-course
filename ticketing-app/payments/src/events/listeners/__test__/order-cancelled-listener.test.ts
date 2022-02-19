01692db83765bb3251fff1af199c5ce123d641d5
Rob de Boer <roblkdeboer@gmail.com>
2022-02-19 16:38:28 +0800

    Order cancellled event listener and tests

---
+++ ticketing-app/payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCancelleddListener } from '../order-cancelled-listener';
+import { OrderCancelledEvent, OrderStatus } from '@robtickets/common';
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  const listener = new OrderCancelleddListener(natsWrapper.client);
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    price: 10,
+    userId: '123456',
+    version: 0,
+  });
+  await order.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: order.version + 1,
+    ticket: {
+      id: 'abcdefg',
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, order };
+};
+
+it('updates the status of the order', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
---
+++ ticketing-app/payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,32 @@
+import {
+  Listener,
+  OrderCancelledEvent,
+  Subjects,
+  OrderStatus,
+} from '@robtickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+
+export class OrderCancelleddListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({ status: OrderStatus.Cancelled });
+
+    await order.save();
+
+    msg.ack();
+  }
+}
