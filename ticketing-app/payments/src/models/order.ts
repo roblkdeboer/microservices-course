e32a7a3ae589f75fefe97aa6009e60a126056413
Rob de Boer <roblkdeboer@gmail.com>
2022-02-19 16:12:57 +0800

    Add optimistic concurrency handling for payments service

--- ticketing-app/payments/src/models/order.ts
+++ ticketing-app/payments/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@robtickets/common';
 
 interface OrderAttrs {
@@ -45,6 +46,10 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+// Optimistic concurrency to update events in order
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
