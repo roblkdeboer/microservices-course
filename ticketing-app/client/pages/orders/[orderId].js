3d9edda85688c5924bac19e9c042b80238898254
Rob de Boer <roblkdeboer@gmail.com>
2022-02-22 23:11:37 +0800

    Add order expiration countdown

--- ticketing-app/client/pages/orders/[orderId].js
+++ ticketing-app/client/pages/orders/[orderId].js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const orderShow = () => {
-  return <div>Order Show</div>;
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - newDate();
+
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    // Stop timer when navigating away from the component
+    return () => {
+      clearInterval(timerId);
+    };
+    // Empty array means it will only run once, when the component first loads
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return <div>Time left to pay: {timeLeft} seconds</div>;
 };
 
-export default orderShow;
+TicketShow.getInitialProps = async (context, client) => {
+  // orderId because that is what the file is called
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
