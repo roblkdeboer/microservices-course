9b13a50d824a5083efe51be4aa0a27eae48b5e71
Rob de Boer <roblkdeboer@gmail.com>
2022-02-24 00:41:42 +0800

    Edit start up message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,7 +9,7 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 // Function that tries to connect to DB, if it works then it listens on the port
 // If not, it throws an error
 const start = async () => {
-  console.log('Starting!');
+  console.log('Starting!!');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
